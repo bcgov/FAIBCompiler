1e31b01aa2aa7493908d32adeeb9616be6523e5a
Luo <Yong.Luo@gov.bc.ca>
2021-04-15 13:22:00 -0700

    [updateSpatial] read map from map folder

--- R/updateSpatial.R
+++ R/updateSpatial.R
@@ -14,7 +14,7 @@
 #' @importFrom FAIBBase UTM_Convertor getSpatial
 #' @export
 #' @author Yong Luo
-updateSpatial <- function(samplesites, mapPath, mapTimes){
+updateSpatial <- function(samplesites, mapPath){
   samplesites_Loc <- unique(samplesites[!is.na(IP_UTM) & !is.na(IP_NRTH) & !is.na(IP_EAST),
                                         .(SITE_IDENTIFIER,
                                           IP_UTM, IP_NRTH, IP_EAST)],
@@ -33,47 +33,50 @@ updateSpatial <- function(samplesites, mapPath, mapTimes){
                                            class = "table")
   names(samplesites_Loc_longlat) <- c("SITE_IDENTIFIER", "Longitude", "Latitude")
 
-  becmap <- readRDS(file.path(mapPath, paste0("BEC_", mapTimes$currenttime, ".rds")))
+
+  becmap <- readRDS(dir(mapPath, pattern = "BEC_map", full.names = TRUE))
+
   samplesites_loc_bec <- FAIBBase::getSpatial(pointID = samplesites_Loc$SITE_IDENTIFIER,
                                     zone = samplesites_Loc$IP_UTM,
                                     northing = samplesites_Loc$IP_NRTH,
                                     easting = samplesites_Loc$IP_EAST,
-                                    spatialMap = becmap,
+                                    spatialMap = becmap$map,
                                     spatialAttribute = "bec")
   names(samplesites_loc_bec) <- c("SITE_IDENTIFIER", "BEC", "BEC_SBZ", "BEC_VAR")
 
-  tsamap <- readRDS(file.path(mapPath, paste0("TSA_", mapTimes$currenttime, ".rds")))
+  tsamap <- readRDS(dir(mapPath, "TSA_", full.names = TRUE))
   samplesites_loc_tsa <- FAIBBase::getSpatial(pointID = samplesites_Loc$SITE_IDENTIFIER,
                                     zone = samplesites_Loc$IP_UTM,
                                     northing = samplesites_Loc$IP_NRTH,
                                     easting = samplesites_Loc$IP_EAST,
-                                    spatialMap = tsamap,
+                                    spatialMap = tsamap$map,
                                     spatialAttribute = "tsa")
   names(samplesites_loc_tsa) <- c("SITE_IDENTIFIER", "TSA", "TSA_DESC")
 
-  fizmap <- readRDS(file.path(mapPath, paste0("FIZ_", mapTimes$fiz_time, ".rds")))
+  fizmap <- readRDS(dir(mapPath, "FIZ_", full.names = TRUE))
   samplesites_loc_fiz <- FAIBBase::getSpatial(pointID = samplesites_Loc$SITE_IDENTIFIER,
                                     zone = samplesites_Loc$IP_UTM,
                                     northing = samplesites_Loc$IP_NRTH,
                                     easting = samplesites_Loc$IP_EAST,
-                                    spatialMap = fizmap,
+                                    spatialMap = fizmap$map,
                                     spatialAttribute = "fiz")
   names(samplesites_loc_fiz) <- c("SITE_IDENTIFIER", "FIZ")
 
-  tflmap <- readRDS(file.path(mapPath, paste0("TFL_", mapTimes$tfl_time, ".rds")))
+  tflmap <- readRDS(dir(mapPath, "TFL_", full.names = TRUE))
   samplesites_loc_tfl <- FAIBBase::getSpatial(pointID = samplesites_Loc$SITE_IDENTIFIER,
                                     zone = samplesites_Loc$IP_UTM,
                                     northing = samplesites_Loc$IP_NRTH,
                                     easting = samplesites_Loc$IP_EAST,
-                                    spatialMap = tflmap,
+                                    spatialMap = tflmap$map,
                                     spatialAttribute = "tfl")
   names(samplesites_loc_tfl) <- c("SITE_IDENTIFIER", "TFL")
-  ownermap <- readRDS(file.path(mapPath, paste0("OWNER_", mapTimes$ownership_time, ".rds")))
+  ownermap <- readRDS(dir(mapPath, "Ownership_", full.names = TRUE))
+
   samplesites_loc_ownership <- FAIBBase::getSpatial(pointID = samplesites_Loc$SITE_IDENTIFIER,
                                           zone = samplesites_Loc$IP_UTM,
                                           northing = samplesites_Loc$IP_NRTH,
                                           easting = samplesites_Loc$IP_EAST,
-                                          spatialMap = ownermap,
+                                          spatialMap = ownermap$map,
                                           spatialAttribute = "OWNERSHIP")
   names(samplesites_loc_ownership)[1] <- c("SITE_IDENTIFIER")
   samplesites <- merge(samplesites, samplesites_Loc_bcalbers,
