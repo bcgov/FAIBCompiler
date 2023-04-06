161356222e569fa4cb43593ce8840db208c97f2f
Luo <Yong.Luo@gov.bc.ca>
2023-04-06 09:32:44 -0700

    [ISMC_DataPrep] corrected dbh tagging limit and sample break point for psp data

--- R/ISMC_DataPrep.R
+++ R/ISMC_DataPrep.R
@@ -166,21 +166,54 @@ ISMC_DataPrep <- function(compilationType,
                 samplemeasurement,
                 by = c("SITE_IDENTIFIER", "VISIT_NUMBER"),
                 all.x = TRUE)
-  ## assign dbh_tagging_limit for NAs
-  ## 1) 9.1cm for measurement year < 1980
-  ## 2) 7.5cm for measurement 1980<=year<=1990
-  ## 2) 4cm for measurement 1990 < year
-  ## based on Rene's previous compiler
-  vi_a[is.na(DBH_TAGGING_LIMIT) &
-         as.numeric(substr(MEAS_DT, 1, 4)) < 1980,
-       DBH_TAGGING_LIMIT := 9.1]
-  vi_a[is.na(DBH_TAGGING_LIMIT) &
-         as.numeric(substr(MEAS_DT, 1, 4)) >= 1980 &
-         as.numeric(substr(MEAS_DT, 1, 4)) <= 1990,
-       DBH_TAGGING_LIMIT := 7.5]
-  vi_a[is.na(DBH_TAGGING_LIMIT) &
-         as.numeric(substr(MEAS_DT, 1, 4)) > 1990,
-       DBH_TAGGING_LIMIT := 4]
+  # ## assign dbh_tagging_limit for NAs
+  # ## 1) 9.1cm for measurement year < 1980
+  # ## 2) 7.5cm for measurement 1980<=year<=1990
+  # ## 2) 4cm for measurement 1990 < year
+  # ## based on Rene's previous compiler
+  # vi_a[, DBH_TAGGING_LIMIT_org := DBH_TAGGING_LIMIT]
+  # vi_a[is.na(DBH_TAGGING_LIMIT) &
+  #        as.numeric(substr(MEAS_DT, 1, 4)) < 1980,
+  #      DBH_TAGGING_LIMIT := 9.1]
+  # vi_a[is.na(DBH_TAGGING_LIMIT) &
+  #        as.numeric(substr(MEAS_DT, 1, 4)) >= 1980 &
+  #        as.numeric(substr(MEAS_DT, 1, 4)) <= 1990,
+  #      DBH_TAGGING_LIMIT := 7.5]
+  # vi_a[is.na(DBH_TAGGING_LIMIT) &
+  #        as.numeric(substr(MEAS_DT, 1, 4)) > 1990 &
+  #        SAMPLE_BREAK_POINT == 2,
+  #      DBH_TAGGING_LIMIT := 2]
+  # vi_a[is.na(DBH_TAGGING_LIMIT) &
+  #        as.numeric(substr(MEAS_DT, 1, 4)) > 1990,
+  #      DBH_TAGGING_LIMIT := 4]
+  ## hard code fix for site = 4042369 visit 3, the break point should be 4
+  vi_a[SITE_IDENTIFIER == 4042369 & VISIT_NUMBER == 3,
+       SAMPLE_BREAK_POINT := 4]
+  ## hard code to fill missing samplebreak point
+  vi_a[substr(MEAS_DT, 1, 4) >= 2020 &
+         is.na(SAMPLE_BREAK_POINT),
+       ':='(SAMPLE_BREAK_POINT = 4,
+            SAMPLE_BREAK_POINT_TYPE = "D")]
+  vi_a[SITE_IDENTIFIER == 4002144 &
+         VISIT_NUMBER == 1,
+       ':='(SAMPLE_BREAK_POINT = 4,
+            SAMPLE_BREAK_POINT_TYPE = "D")]
+  vi_a[SITE_IDENTIFIER == 4044744 &
+         VISIT_NUMBER == 2,
+       ':='(SAMPLE_BREAK_POINT = 4,
+            SAMPLE_BREAK_POINT_TYPE = "D")]
+  vi_a[SITE_IDENTIFIER == 4001722 &
+         VISIT_NUMBER == 3,
+       ':='(SAMPLE_BREAK_POINT = 4,
+            SAMPLE_BREAK_POINT_TYPE = "D")]
+
+  ## see communications between Dan and I on March 07, 2023
+  vi_a[, DBH_TAGGING_LIMIT_org := DBH_TAGGING_LIMIT]
+  vi_a[is.na(DBH_TAGGING_LIMIT),
+       DBH_TAGGING_LIMIT := 2]
+  vi_a[SAMPLE_BREAK_POINT < DBH_TAGGING_LIMIT,
+       ':='(SAMPLE_BREAK_POINT = 4)]
+
   saveRDS(vi_a, file.path(outputPath, "vi_a.rds"))
   plotdetails <- readRDS(dir(inputPath, pattern = "PlotDetails.rds",
                              full.names = TRUE))
@@ -189,7 +222,7 @@ ISMC_DataPrep <- function(compilationType,
                                             CLSTR_ID)],
                        by = c("SITE_IDENTIFIER", "VISIT_NUMBER"),
                        all.x = TRUE)
-
+  plotdetails <- plotdetails[!is.na(CLSTR_ID),]
   # for nonPSP, a sample may contain multiple plots,
   # which is flagged in plot_category code,
   # for the psp, a sample also may contain multiple plots
