814f9936852809ae516cc397c57e98940e01ef32
Luo <Yong.Luo@gov.bc.ca>
2023-06-15 11:56:01 -0700

    [vihPrep] fixed a bug to derive age for extrapolated site trees

--- R/vihPrep.R
+++ R/vihPrep.R
@@ -13,13 +13,13 @@ vihPrep <- function(msmtInterval,
                         tree_last_msmt,
                         by = c("SITE_IDENTIFIER", "PLOT", "TREE_NO"),
                         all.x = TRUE)
-  siteAgeTrees[is.na(PERIOD), PERIOD := 0]
+
 
   siteAgeTrees_last <- siteAgeTrees[VISIT_NUMBER == tree_last_times,
                                     .(SITE_IDENTIFIER,
                                       PLOT,
                                       TREE_NO,
-                                      PERIOD_last = PERIOD,
+                                      MEAS_YR_REF = MEAS_YR,
                                       BORAG_FL_last = BORAG_FL,
                                       BORE_AGE_last = BORE_AGE,
                                       BORED_HT_last = BORED_HT)]
@@ -29,24 +29,25 @@ vihPrep <- function(msmtInterval,
                                "PLOT",
                                "TREE_NO"),
                         all.x = TRUE)
+  siteAgeTrees[MEAS_YR == MEAS_YR_REF,
+               BORED_AGE_FLAG := "REFERENCE"]
+
   siteAgeTrees[is.na(BORED_HT) & is.na(MEAS_COD) &
-                 PERIOD != PERIOD_last,
-               ':='(MEAS_COD = "FROM_LAST",
+                 MEAS_YR != MEAS_YR_REF,
+               ':='(BORED_AGE_FLAG = "ADDED_FROM_REFERENCE",
                     BORED_HT = BORED_HT_last,
-                    BORAG_FL = BORAG_FL_last - (PERIOD_last - PERIOD),
-                    BORE_AGE = BORE_AGE_last - (PERIOD_last - PERIOD))] # the age information is from last measurement
+                    BORAG_FL = BORAG_FL_last - (MEAS_YR_REF - MEAS_YR),
+                    BORE_AGE = BORE_AGE_last - (MEAS_YR_REF - MEAS_YR))] # the age information is from last measurement
   siteAgeTrees[(!is.na(BORED_HT) | !is.na(MEAS_COD)) & ## have measurement flag
-                 PERIOD != PERIOD_last & # not from last measurement
-                 MEAS_COD != "FROM_LAST" & # not from last measurement
+                 MEAS_YR != MEAS_YR_REF & # not from last measurement
+                 BORED_AGE_FLAG != "ADDED_FROM_REFERENCE" & # not from last measurement
                  BORED_HT == BORED_HT_last, # have same measurement height
-               ':='(BORE_AGE_adj = BORE_AGE_last - (PERIOD_last - PERIOD))] # the age information is from last measurement
+               ':='(BORE_AGE_adj = BORE_AGE_last - (MEAS_YR_REF - MEAS_YR))] # the age information is from last measurement
 
   siteAgeTrees[BORE_AGE != BORE_AGE_adj,
-               ':='(MEAS_COD = paste0(MEAS_COD, "-ADJ_FROM_LAST"),
+               ':='(BORED_AGE_FLAG = "CORRECTED_FROM_REFERENCE",
                     BORE_AGE = BORE_AGE_adj)]
-  siteAgeTrees[,':='(PERIOD = NULL,
-                     tree_last_times = NULL,
-                     PERIOD_last = NULL,
+  siteAgeTrees[,':='(tree_last_times = NULL,
                      BORAG_FL_last = NULL,
                      BORE_AGE_last = NULL,
                      BORED_HT_last = NULL,
