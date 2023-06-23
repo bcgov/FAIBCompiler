69fb9635039d2482307ea23e2d50341c3f0210c4
Luo <Yong.Luo@gov.bc.ca>
2023-06-23 10:31:50 -0700

    [vihPrep] standardized process between nonPSP and PSP

--- R/vihPrep.R
+++ R/vihPrep.R
@@ -6,54 +6,80 @@ vihPrep <- function(msmtInterval,
   siteAgeTrees <- merge(siteAgeTrees, msmtInterval,
                         by = "CLSTR_ID",
                         all.x = TRUE)
-  tree_last_msmt <- siteAgeTrees[!is.na(BORED_HT) | !is.na(MEAS_COD),
+  tree_last_msmt <- siteAgeTrees[(!is.na(BORED_HT) | !is.na(AGE_MEASURE_CODE)) &
+                                   (!is.na(BORE_AGE_FLD) | !is.na(BORE_AGE_LAB)) &
+                                   (!AGE_MEASURE_CODE %in% c("PRE", "NOC") | is.na(AGE_MEASURE_CODE)),
                                    .(tree_last_times = max(VISIT_NUMBER)),
                                  by = c("SITE_IDENTIFIER", "PLOT", "TREE_NO")]
   siteAgeTrees <- merge(siteAgeTrees,
                         tree_last_msmt,
                         by = c("SITE_IDENTIFIER", "PLOT", "TREE_NO"),
                         all.x = TRUE)
-
-
   siteAgeTrees_last <- siteAgeTrees[VISIT_NUMBER == tree_last_times,
                                     .(SITE_IDENTIFIER,
                                       PLOT,
                                       TREE_NO,
                                       MEAS_YR_REF = MEAS_YR,
-                                      BORAG_FL_last = BORAG_FL,
-                                      BORE_AGE_last = BORE_AGE,
+                                      BORAG_FL_last = BORE_AGE_FLD,
+                                      BORE_AGE_last = BORE_AGE_LAB,
                                       BORED_HT_last = BORED_HT)]
+  siteAgeTrees_last[!is.na(BORAG_FL_last) & is.na(BORE_AGE_last),
+                    lab_age_remove := TRUE]
   siteAgeTrees <- merge(siteAgeTrees,
                         siteAgeTrees_last,
                         by = c("SITE_IDENTIFIER",
                                "PLOT",
                                "TREE_NO"),
                         all.x = TRUE)
+  siteAgeTrees[!is.na(BORE_AGE_LAB) & lab_age_remove == TRUE,
+               BORE_AGE_LAB := NA]
+  # choose last fld age over prevous lab age as the reference to correct
   siteAgeTrees[MEAS_YR == MEAS_YR_REF,
-               BORED_AGE_FLAG := "REFERENCE"]
+               BORED_AGE_FLAG := "Reference"]
 
-  siteAgeTrees[is.na(BORED_HT) & is.na(MEAS_COD) &
+  siteAgeTrees[is.na(BORED_HT) & is.na(AGE_MEASURE_CODE) &
                  MEAS_YR != MEAS_YR_REF,
-               ':='(BORED_AGE_FLAG = "ADDED_FROM_REFERENCE",
+               ':='(BORED_AGE_FLAG = "Added from Reference",
                     BORED_HT = BORED_HT_last,
-                    BORAG_FL = BORAG_FL_last - (MEAS_YR_REF - MEAS_YR),
-                    BORE_AGE = BORE_AGE_last - (MEAS_YR_REF - MEAS_YR))] # the age information is from last measurement
-  siteAgeTrees[(!is.na(BORED_HT) | !is.na(MEAS_COD)) & ## have measurement flag
+                    BORE_AGE_FLD = BORAG_FL_last - (MEAS_YR_REF - MEAS_YR),
+                    BORE_AGE_LAB = BORE_AGE_last - (MEAS_YR_REF - MEAS_YR))] # the age information is from last measurement
+  siteAgeTrees[(!is.na(BORED_HT) | !is.na(AGE_MEASURE_CODE)) & ## have measurement flag
                  MEAS_YR != MEAS_YR_REF & # not from last measurement
-                 BORED_AGE_FLAG != "ADDED_FROM_REFERENCE" & # not from last measurement
-                 BORED_HT == BORED_HT_last, # have same measurement height
-               ':='(BORE_AGE_adj = BORE_AGE_last - (MEAS_YR_REF - MEAS_YR))] # the age information is from last measurement
+                 is.na(BORED_AGE_FLAG), # not added
+               ':='(BORE_AGE_adj = BORE_AGE_last - (MEAS_YR_REF - MEAS_YR),
+                    BORE_FLD_adj = BORAG_FL_last - (MEAS_YR_REF - MEAS_YR))] # the age information is from last measurement
+  siteAgeTrees[(BORE_AGE_LAB != BORE_AGE_adj |
+                 is.na(BORE_AGE_LAB)) &
+                 !is.na(BORE_AGE_adj) &
+                 is.na(BORED_AGE_FLAG),
+               ':='(BORED_AGE_FLAG = "LAB_AGE Corrected from Reference",
+                    BORE_AGE_LAB = BORE_AGE_adj)]
+  siteAgeTrees[BORED_AGE_FLAG == "LAB_AGE Corrected from Reference" &
+                 (BORED_HT_last != BORED_HT | is.na(BORED_HT)),
+               ':='(BORED_HT = BORED_HT_last, # bored ht set to reference
+                    BORED_AGE_FLAG = "LAB_AGE Corrected from Reference, BORED_HT set to Reference")]
+  siteAgeTrees[((BORE_AGE_FLD != BORE_FLD_adj |
+                 is.na(BORE_AGE_FLD))) &
+                 !is.na(BORE_FLD_adj) &
+                 is.na(BORED_AGE_FLAG),
+               ':='(BORED_AGE_FLAG = "FLD_AGE Corrected from Reference",
+                    BORE_AGE_FLD = BORE_FLD_adj)]
+  siteAgeTrees[BORED_AGE_FLAG == "FLD_AGE Corrected from Reference" &
+                 (BORED_HT_last != BORED_HT | is.na(BORED_HT)),
+               ':='(BORED_HT = BORED_HT_last, # bored ht set to reference
+                    BORED_AGE_FLAG = "FLD_AGE Corrected from Reference, BORED_HT set to Reference")]
 
-  siteAgeTrees[BORE_AGE != BORE_AGE_adj,
-               ':='(BORED_AGE_FLAG = "CORRECTED_FROM_REFERENCE",
-                    BORE_AGE = BORE_AGE_adj)]
   siteAgeTrees[,':='(tree_last_times = NULL,
                      BORAG_FL_last = NULL,
                      BORE_AGE_last = NULL,
                      BORED_HT_last = NULL,
-                     BORE_AGE_adj = NULL)]
-  siteAgeTrees[BORE_AGE <= 0, BORE_AGE := NA]
-  siteAgeTrees[BORAG_FL <= 0, BORAG_FL := NA]
+                     BORE_AGE_adj = NULL,
+                     BORE_FLD_adj = NULL,
+                     lab_age_remove = NULL)]
+  siteAgeTrees[BORE_AGE_LAB <= 0, BORE_AGE_LAB := NA]
+  siteAgeTrees[BORE_AGE_FLD <= 0, BORE_AGE_FLD := NA]
+  siteAgeTrees[BORED_HT > 0.1 & !is.na(TOTAL_AG),
+               TOTAL_AG := NA] # the bore height higher than 0.1 does not give total age
   return(siteAgeTrees)
 }
 
