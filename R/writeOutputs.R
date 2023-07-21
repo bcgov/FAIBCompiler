80f693b964b91144854776369e7ae319badb2db9
Luo <Yong.Luo@gov.bc.ca>
2023-07-21 13:21:40 -0700

    [writeOutput] writeOutput function was created

---
+++ R/writeOutputs.R
@@ -0,0 +1,34 @@
+
+
+
+writeOutputs <- function(fileTable,
+                         compilationType,
+                         compilationPaths){
+  for (indirow in 1:nrow(fileTable)) {
+    thedata <- readRDS(file.path(fileTable$folderName[indirow],
+                                 paste0(fileTable$fileName[indirow], ".rds")))
+    write.csv(thedata,
+              file.path(fileTable$folderName[indirow],
+                        paste0(fileTable$fileName[indirow], ".csv")),
+              na = "",
+              row.names = FALSE)
+    if(fileTable$folderName[indirow] == compilationPaths$compilation_db &
+       compilationType == "nonPSP"){
+      write.xlsx(thedata,
+                 file.path(fileTable$folderName[indirow],
+                           paste0(fileTable$fileName[indirow], ".xlsx")),
+                 overwrite = TRUE)
+    } else if (fileTable$folderName[indirow] == compilationPaths$compilation_db &
+               compilationType == "PSP" &
+               !(fileTable$fileName[indirow] %in% c("treelist", "compiled_vi_c"))){
+      write.xlsx(thedata,
+                 file.path(fileTable$folderName[indirow],
+                           paste0(fileTable$fileName[indirow], ".xlsx")),
+                 overwrite = TRUE)
+    }
+    rm(thedata)
+    gc()
+  }
+}
+
+
