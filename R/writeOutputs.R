bb7004c395ce99d670ddd1ced53567cebdf1ec3b
Luo <Yong.Luo@gov.bc.ca>
2023-08-02 14:34:02 -0700

    [writeOutput] stop writing xlsx files

--- R/writeOutputs.R
+++ R/writeOutputs.R
@@ -12,20 +12,20 @@ writeOutputs <- function(fileTable,
                         paste0(fileTable$fileName[indirow], ".csv")),
               na = "",
               row.names = FALSE)
-    if(fileTable$folderName[indirow] == compilationPaths$compilation_db &
-       compilationType == "nonPSP"){
-      write.xlsx(thedata,
-                 file.path(fileTable$folderName[indirow],
-                           paste0(fileTable$fileName[indirow], ".xlsx")),
-                 overwrite = TRUE)
-    } else if (fileTable$folderName[indirow] == compilationPaths$compilation_db &
-               compilationType == "PSP" &
-               !(fileTable$fileName[indirow] %in% c("treelist", "compiled_vi_c"))){
-      write.xlsx(thedata,
-                 file.path(fileTable$folderName[indirow],
-                           paste0(fileTable$fileName[indirow], ".xlsx")),
-                 overwrite = TRUE)
-    }
+    # if(fileTable$folderName[indirow] == compilationPaths$compilation_db &
+    #    compilationType == "nonPSP"){
+    #   write.xlsx(thedata,
+    #              file.path(fileTable$folderName[indirow],
+    #                        paste0(fileTable$fileName[indirow], ".xlsx")),
+    #              overwrite = TRUE)
+    # } else if (fileTable$folderName[indirow] == compilationPaths$compilation_db &
+    #            compilationType == "PSP" &
+    #            !(fileTable$fileName[indirow] %in% c("treelist", "compiled_vi_c"))){
+    #   write.xlsx(thedata,
+    #              file.path(fileTable$folderName[indirow],
+    #                        paste0(fileTable$fileName[indirow], ".xlsx")),
+    #              overwrite = TRUE)
+    # }
     rm(thedata)
     gc()
   }
