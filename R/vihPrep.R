80cec69e8a325b07f9455ec18a57eb1996493016
Luo <Yong.Luo@gov.bc.ca>
2023-09-13 12:59:25 -0700

    [vihPrep] fixed a bug to allow all the trees have meas_yr_ref

--- R/vihPrep.R
+++ R/vihPrep.R
@@ -9,7 +9,7 @@ vihPrep <- function(msmtInterval,
   tree_last_msmt <- siteAgeTrees[(!is.na(BORED_HT) | !is.na(AGE_MEASURE_CODE)) &
                                    (!is.na(BORE_AGE_FLD) | !is.na(BORE_AGE_LAB)) &
                                    (!AGE_MEASURE_CODE %in% c("PRE", "NOC") | is.na(AGE_MEASURE_CODE)),
-                                   .(tree_last_times = max(VISIT_NUMBER)),
+                                 .(tree_last_times = max(VISIT_NUMBER)),
                                  by = c("SITE_IDENTIFIER", "PLOT", "TREE_NO")]
   siteAgeTrees <- merge(siteAgeTrees,
                         tree_last_msmt,
@@ -49,7 +49,7 @@ vihPrep <- function(msmtInterval,
                ':='(BORE_AGE_adj = BORE_AGE_last - (MEAS_YR_REF - MEAS_YR),
                     BORE_FLD_adj = BORAG_FL_last - (MEAS_YR_REF - MEAS_YR))] # the age information is from last measurement
   siteAgeTrees[(BORE_AGE_LAB != BORE_AGE_adj |
-                 is.na(BORE_AGE_LAB)) &
+                  is.na(BORE_AGE_LAB)) &
                  !is.na(BORE_AGE_adj) &
                  is.na(BORED_AGE_FLAG),
                ':='(BORED_AGE_FLAG = "LAB_AGE Corrected from Reference",
@@ -59,7 +59,7 @@ vihPrep <- function(msmtInterval,
                ':='(BORED_HT = BORED_HT_last, # bored ht set to reference
                     BORED_AGE_FLAG = "LAB_AGE Corrected from Reference, BORED_HT set to Reference")]
   siteAgeTrees[((BORE_AGE_FLD != BORE_FLD_adj |
-                 is.na(BORE_AGE_FLD))) &
+                   is.na(BORE_AGE_FLD))) &
                  !is.na(BORE_FLD_adj) &
                  is.na(BORED_AGE_FLAG),
                ':='(BORED_AGE_FLAG = "FLD_AGE Corrected from Reference",
@@ -80,6 +80,19 @@ vihPrep <- function(msmtInterval,
   siteAgeTrees[BORE_AGE_FLD <= 0, BORE_AGE_FLD := NA]
   siteAgeTrees[BORED_HT > 0.1 & !is.na(TOTAL_AG),
                TOTAL_AG := NA] # the bore height higher than 0.1 does not give total age
+
+  siteAgeTrees_noref <- siteAgeTrees[is.na(MEAS_YR_REF),
+                                     .(lastmearyr = max(MEAS_YR)),
+                                     by = c("SITE_IDENTIFIER", "PLOT", "TREE_NO")]
+  siteAgeTrees <- merge(siteAgeTrees,
+                        siteAgeTrees_noref,
+                        by = c("SITE_IDENTIFIER", "PLOT", "TREE_NO"),
+                        all.x = TRUE)
+  siteAgeTrees[is.na(MEAS_YR_REF),
+               MEAS_YR_REF := lastmearyr]
+  siteAgeTrees[is.na(BORED_AGE_FLAG) & MEAS_YR == MEAS_YR_REF,
+               BORED_AGE_FLAG := "Reference"]
+  siteAgeTrees[, lastmearyr := NULL]
   return(siteAgeTrees)
 }
 
