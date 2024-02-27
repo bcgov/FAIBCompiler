b1a8300eb4d4dd1c72454d2da20d21d8ef979446
Luo <Yong.Luo@gov.bc.ca>
2024-02-27 11:13:23 -0800

    [updateSA_vegcomp] added LAYER_ID from rank 1 layer

--- R/updateSA_vegcomp.R
+++ R/updateSA_vegcomp.R
@@ -37,57 +37,61 @@ updateSA_vegcomp <- function(compilationType,
                             paste0("SA_VEGCOMP_", compilationType, ".rds")))){
     need_extract <- TRUE
     sampleSites_valid <- sampleSites[!is.na(BC_ALBERS_X) &
-                                                     !is.na(BC_ALBERS_Y),]
+                                       !is.na(BC_ALBERS_Y),]
   } else {
     sample_site_valid_all_prev <- readRDS(file.path(coeffPath,
                                                     paste0("SA_VEGCOMP_", compilationType, ".rds")))
     if(sample_site_valid_all_prev$vegCompVersion != vegcompversion){
       need_extract <- TRUE
       sampleSites_valid <- sampleSites[!(is.na(BC_ALBERS_X) &
-                                                         is.na(BC_ALBERS_Y)),]
+                                           is.na(BC_ALBERS_Y)),]
     } else {
       sampleSites_valid_prev <- sample_site_valid_all_prev$SAFromVegComp
       sampleSites <- merge(sampleSites,
-                                  sampleSites_valid_prev,
-                                  by = "SITE_IDENTIFIER",
-                                  all.x = TRUE)
+                           sampleSites_valid_prev,
+                           by = "SITE_IDENTIFIER",
+                           all.x = TRUE)
       sampleSites[is.na(x_BCAlbers), x_BCAlbers := -1] # force NAs to -1
       sampleSites[is.na(y_BCAlbers), y_BCAlbers := -1] # force NAs to -1
       sampleSites_valid <- sampleSites[!(is.na(BC_ALBERS_X) &
-                                                         is.na(BC_ALBERS_Y)) &
-                                                       !(BC_ALBERS_X == x_BCAlbers &
-                                                           BC_ALBERS_Y == y_BCAlbers),]
+                                           is.na(BC_ALBERS_Y)) &
+                                         !(BC_ALBERS_X == x_BCAlbers &
+                                             BC_ALBERS_Y == y_BCAlbers),]
       sampleSites[,':='(x_BCAlbers = NULL,
-                               y_BCAlbers = NULL)]
+                        y_BCAlbers = NULL)]
       if(nrow(sampleSites_valid) > 0){
         need_extract <- TRUE
         additional <- TRUE
         sampleSites_valid_prev_keep <- sampleSites_valid_prev[!(SITE_IDENTIFIER %in% sampleSites_valid$SITE_IDENTIFIER),]
         sampleSites[,':='(FEATURE_ID = NULL,
-                                 PROJ_AGE_1 = NULL,
-                                 PROJECTED_DATE = NULL)]
+                          PROJ_AGE_1 = NULL,
+                          PROJECTED_DATE = NULL)]
       }
     }
   }
   if(need_extract){
     sampleSites_valid <- extractVegComp_byLoc(bcgwUserName = bcgwUserName,
-                                                     bcgwPassword = bcgwPassword,
-                                                     locationID = sampleSites_valid$SITE_IDENTIFIER,
-                                                     x_BCAlbers = sampleSites_valid$BC_ALBERS_X,
-                                                     y_BCAlbers = sampleSites_valid$BC_ALBERS_Y,
-                                                     extractAttributes = c("Feature_ID", "PROJ_AGE_1", "PROJECTED_DATE"))
-    sampleSites_valid <- sampleSites_valid[,.(SITE_IDENTIFIER = locationID,
-                                                            x_BCAlbers, y_BCAlbers,
-                                                            FEATURE_ID, PROJ_AGE_1, PROJECTED_DATE)]
+                                              bcgwPassword = bcgwPassword,
+                                              locationID = sampleSites_valid$SITE_IDENTIFIER,
+                                              x_BCAlbers = sampleSites_valid$BC_ALBERS_X,
+                                              y_BCAlbers = sampleSites_valid$BC_ALBERS_Y,
+                                              extractAttributes = c("Feature_ID", "FOR_COVER_RANK_CD",
+                                                                    "PROJ_AGE_1",
+                                                                    "PROJECTED_DATE", "LAYER_ID"))
+    sampleSites_valid <- sampleSites_valid[FOR_COVER_RANK_CD %in% c(1, NA), # make sure this is from rank 1 layer
+                                           .(SITE_IDENTIFIER = locationID,
+                                             x_BCAlbers, y_BCAlbers,
+                                             FEATURE_ID, PROJ_AGE_1,
+                                             PROJECTED_DATE, LAYER_ID)]
     if(additional){
       sampleSites_valid <- rbind(sampleSites_valid,
-                                        sampleSites_valid_prev_keep)
+                                 sampleSites_valid_prev_keep)
     }
     sampleSites <- merge(sampleSites,
-                                sampleSites_valid[,.(SITE_IDENTIFIER, FEATURE_ID,
-                                                            PROJ_AGE_1, PROJECTED_DATE)],
-                                by = "SITE_IDENTIFIER",
-                                all.x = TRUE)
+                         sampleSites_valid[,.(SITE_IDENTIFIER, FEATURE_ID,LAYER_ID,
+                                              PROJ_AGE_1, PROJECTED_DATE)],
+                         by = "SITE_IDENTIFIER",
+                         all.x = TRUE)
     saveRDS(list(vegCompVersion = vegcompversion,
                  SAFromVegComp = sampleSites_valid),
             file.path(coeffPath,
@@ -96,16 +100,17 @@ updateSA_vegcomp <- function(compilationType,
   }
   sampleMsmts <- merge(sampleMsmts,
                        sampleSites[,.(SITE_IDENTIFIER,
-                                         PROJ_AGE_1, PROJECTED_DATE)],
-                   by = "SITE_IDENTIFIER",
-                   all.x = TRUE)
+                                      PROJ_AGE_1, PROJECTED_DATE)],
+                       by = "SITE_IDENTIFIER",
+                       all.x = TRUE)
   sampleMsmts[,':='(sa_ref = PROJ_AGE_1,
-                year_ref = as.numeric(substr(PROJECTED_DATE, 1, 4)))]
-  sampleMsmts[, SA_VEGCOMP := sa_ref - (year_ref - MEAS_YR)]
+                    VEGCOMP_YEAR = as.numeric(substr(PROJECTED_DATE, 1, 4)))]
+  sampleMsmts[, SA_VEGCOMP := sa_ref - (VEGCOMP_YEAR - MEAS_YR)]
   sampleMsmts[, ':='(sa_ref = NULL,
-                 year_ref = NULL)]
-return(list(sampleSites = sampleSites,
-            sampleMsmts = sampleMsmts))
+                     PROJ_AGE_1 = NULL,
+                     PROJECTED_DATE = NULL)]
+  return(list(sampleSites = sampleSites,
+              sampleMsmts = sampleMsmts))
 }
 
 
